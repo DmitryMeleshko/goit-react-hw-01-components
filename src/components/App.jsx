ae34e121cf0f7c3e1bbe4514bb2a8b8e90433b1e
DmitryMeleshko <meleshko.triolcorp@gmail.com>
2023-04-09 19:43:14 +0300

    making react-hw-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './Friend/FriendList';
+import { TransactionHistory } from './Transactions/Transaction';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './Friend/friends.json';
+import transactions from './Transactions/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +19,16 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
