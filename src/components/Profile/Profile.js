54953c554d0a6f4818bf0d9f4a5149bf404730b9
DmitryMeleshko <meleshko.triolcorp@gmail.com>
2023-04-11 13:14:58 +0300

    fixed Profile (props)

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -37,7 +37,11 @@ Profile.propTypes ={
   tag: PropTypes.string,
   location: PropTypes.string,
   avatar: PropTypes.string,
-  followers: PropTypes.number,
-  views: PropTypes.number,
-  likes: PropTypes.number,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
 };
